234f933f8bd28e9987e799fa7dea830ca6b27d7b
sylcym <prusak.syl@gmail.com>
2025-11-15 17:04:46 +0100

    add js header

--- manage-landing-page-master/script.js
+++ manage-landing-page-master/script.js
@@ -1,2 +1,26 @@
+const btnOpen = document.querySelector(".btn-open");
+const btnClose = document.querySelector(".btn-close");
+const nav = document.querySelector(".navigation");
+const overlay = document.querySelector("#overlay");
 
+btnOpen.addEventListener("click", () => {
+  nav.classList.add("active");
+  overlay.classList.add("show");
+  btnClose.classList.add("active");
+  btnOpen.classList.add("hidden");
+});
+
+btnClose.addEventListener("click", () => {
+  nav.classList.remove("active");
+  overlay.classList.remove("show");
+  btnClose.classList.remove("active");
+  btnOpen.classList.remove("hidden");
+});
+
+overlay.addEventListener("click", () => {
+  nav.classList.remove("active");
+  overlay.classList.remove("show");
+  btnClose.classList.remove("active");
+  btnOpen.classList.remove("hidden");
+});
 
