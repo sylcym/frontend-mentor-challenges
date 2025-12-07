2dcd9b9432de57e9478f03873893ced1ca496fab
sylcym <prusak.syl@gmail.com>
2025-12-07 14:02:54 +0100

    Code refactoring

--- manage-landing-page-master/script.js
+++ manage-landing-page-master/script.js
@@ -1,89 +1,86 @@
-const btnOpen = document.querySelector(".btn-open");
-const btnClose = document.querySelector(".btn-close");
-const nav = document.querySelector(".navigation");
-const overlay = document.querySelector("#overlay");
+window.addEventListener('load', () => {
+  const btnOpen = document.querySelector(".btn-open");
+  const btnClose = document.querySelector(".btn-close");
+  const nav = document.querySelector(".navigation");
+  const overlay = document.querySelector("#overlay");
+  const form = document.querySelector(".newsletter");
+  const input = document.querySelector(".input");
+  const errorMessage = document.querySelector(".error-message");
+  const btnSubmit = document.querySelector(".btn-footer");
+  const track = document.querySelector('.carousel-track');
+  const testimonials = Array.from(track.children);
+  const dots = Array.from(document.querySelectorAll('.dot'));
 
-// const form = document.querySelector(".newsletter");
-// const input = document.querySelector(".input");
-// const errorMessage = document.querySelector(".error-message");
-const form = document.querySelector(".newsletter");
-const input = form.querySelector(".input");
-const errorMessage = form.querySelector(".error-message");
-const btnSubmit = form.querySelector(".btn-footer");
+  let currentIndex = 0;
 
-const track = document.querySelector('.carousel-track');
-const testimonials = Array.from(track.children);
-const dots = Array.from(document.querySelectorAll('.dot'));
+  function isEmail(value) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+  }
 
-let currentIndex = 0;
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
 
+    const emailValue = input.value.trim();
 
-function goToSlide(index) {
-  const slide = testimonials[index];
-  slide.scrollIntoView({
-    behavior: 'smooth',
-    inline: 'center',
-    block: 'nearest'
-  });
+    if (!isEmail(emailValue)) {
+      input.classList.add("error");
+      errorMessage.hidden = false;
+      input.value = "";
+      input.focus();
 
-  dots.forEach(dot => dot.classList.remove('active'));
-  dots[index].classList.add('active');
+      return;
+    }
 
-  currentIndex = index;
-}
-
-dots.forEach((dot, index) => {
-  dot.addEventListener('click', () => {
-    goToSlide(index);
-  });
-});
-
-goToSlide(0);
-
-
-btnOpen.addEventListener("click", () => {
-  nav.classList.add("active");
-  overlay.classList.add("show");
-  btnClose.classList.add("active");
-  btnOpen.classList.add("hidden");
-});
-
-btnClose.addEventListener("click", () => {
-  nav.classList.remove("active");
-  overlay.classList.remove("show");
-  btnClose.classList.remove("active");
-  btnOpen.classList.remove("hidden");
-});
-
-overlay.addEventListener("click", () => {
-  nav.classList.remove("active");
-  overlay.classList.remove("show");
-  btnClose.classList.remove("active");
-  btnOpen.classList.remove("hidden");
-});
-
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  console.log('ok')
-
-  const emailValue = input.value.trim();
-  const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue);
-
-  if (!validEmail) {
-    input.classList.add("error");
-    errorMessage.hidden = false;
-    input.focus();
-  } else {
     input.classList.remove("error");
     errorMessage.hidden = true;
+    btnSubmit.disabled = true;
 
-    console.log("Email OK:", emailValue);
+    setTimeout(() => {
+      input.value = "";
+      btnSubmit.disabled = false;
+    }, 5000);
+  });
 
-    // czyścimy input dopiero PO pozytywnej walidacji
-    input.value = "";
+  function goToSlide(index) {
+    const slide = testimonials[index];
+    slide.scrollIntoView({
+      behavior: 'smooth',
+      inline: 'center',
+      block: 'nearest'
+    });
+
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[index].classList.add('active');
+
+    currentIndex = index;
   }
-});
 
+  dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+      goToSlide(index);
+    });
+  });
 
+  goToSlide(0);
 
+  btnOpen.addEventListener("click", () => {
+    nav.classList.add("active");
+    overlay.classList.add("show");
+    btnClose.classList.add("active");
+    btnOpen.classList.add("hidden");
+  });
+
+  btnClose.addEventListener("click", () => {
+    nav.classList.remove("active");
+    overlay.classList.remove("show");
+    btnClose.classList.remove("active");
+    btnOpen.classList.remove("hidden");
+  });
+
+  overlay.addEventListener("click", () => {
+    nav.classList.remove("active");
+    overlay.classList.remove("show");
+    btnClose.classList.remove("active");
+    btnOpen.classList.remove("hidden");
+  });
+})
