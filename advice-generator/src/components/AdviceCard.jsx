2a82ac724811d80ac38a22e98f5dd70b9ba94287
sylcym <prusak.syl@gmail.com>
2026-03-12 09:51:43 +0100

    fetch advice from API on button click

--- advice-generator/src/components/AdviceCard.jsx
+++ advice-generator/src/components/AdviceCard.jsx
@@ -4,11 +4,21 @@ function AdviceCard() {
   const [advice, setAdvice] = useState("It is easy to sit up and take notice.")
   const [id, setId] = useState(117)
 
+  async function getAdvice() {
+    const response = await fetch("https://api.adviceslip.com/advice")
+    const data = await response.json()
+
+    setAdvice(data.slip.advice)
+    setId(data.slip.id)
+  }
+
   return (
     <div>
       <p>Advice #{id}</p>
       <h2>"{advice}"</h2>
-      <button>Get Advice</button>
+      <button onClick={getAdvice}>
+        Get Advice
+      </button>
     </div>
   )
 }
