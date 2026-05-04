d79302dfa18e20aaa37af0a8a8fca4751e5a180c
sylcym <prusak.syl@gmail.com>
2026-05-04 13:39:11 +0200

    Add pricing component project

---
+++ pricing-component/src/App.jsx
@@ -0,0 +1,11 @@
+import PricingCard from "./features/pricing/components/PricingCard";
+
+function App() {
+  return (
+    <div className="app">
+      <PricingCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ pricing-component/src/features/pricing/components/PricingCard.jsx
@@ -0,0 +1,9 @@
+import styles from "./PricingCard.module.css";
+
+export default function PricingCard() {
+  return (
+    <div className={styles.card}>
+      <h2>Pricing</h2>
+    </div>
+  );
+}
