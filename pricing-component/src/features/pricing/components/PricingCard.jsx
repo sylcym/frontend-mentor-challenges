5407c9543774d6c490dda4b25fcafd277e2eb77b
sylcym <prusak.syl@gmail.com>
2026-05-05 10:40:46 +0200

    feat: add pricing toggle with animation and final UI polish

--- pricing-component/src/features/pricing/components/PricingCard.jsx
+++ pricing-component/src/features/pricing/components/PricingCard.jsx
@@ -1,24 +1,68 @@
 import styles from "./PricingCard.module.css";
+import { useState } from "react";
 
 export default function PricingCard() {
+  const [isYearly, setIsYearly] = useState(true);
+
+  const getPrice = (plan) =>
+    isYearly ? plan.yearly : plan.monthly;
+
+  const pricing = {
+    basic: {
+      monthly: 19.99,
+      yearly: 199.99,
+    },
+    professional: {
+      monthly: 24.99,
+      yearly: 249.99,
+    },
+    master: {
+      monthly: 39.99,
+      yearly: 399.99,
+    },
+  };
+
+  function togglePricing() {
+    setIsYearly((prev) => !prev);
+  }
+
   return (
     <div className={styles.card}>
       <h2 className={styles.title}>Our Pricing</h2>
 
       <div className={styles.toggle}>
-        <span>Annually</span>
-        <button className={styles.switch}></button>
-        <span>Monthly</span>
+        <span
+          onClick={() => setIsYearly(true)}
+          className={!isYearly ? styles.active : ""}
+        >
+          Annually
+        </span>
+
+        <button
+          className={styles.switch}
+          onClick={togglePricing}
+        >
+          <div
+            className={`${styles.thumb} ${!isYearly ? styles.thumbRight : ""
+              }`}
+          />
+        </button>
+
+        <span
+          onClick={() => setIsYearly(false)}
+          className={isYearly ? styles.active : ""}
+        >
+          Monthly
+        </span>
       </div>
 
       <div className={styles.cardsWrapper}>
-        {/* <div className={styles.plan}>Basic</div> */}
         <div className={styles.plan}>
           <h3 className={styles.planName}>Basic</h3>
 
           <p className={styles.price}>
             <span className={styles.currency}>$</span>
-            19.99
+            {getPrice(pricing.basic)}
           </p>
 
           <ul className={styles.features}>
@@ -29,13 +73,12 @@ export default function PricingCard() {
 
           <button className={styles.button}>Learn More</button>
         </div>
-        {/* <div className={styles.planFeatured}>Professional</div> */}
         <div className={styles.planFeatured} >
           <h3 className={styles.planName}>Professional</h3>
 
           <p className={styles.price}>
             <span className={styles.currency}>$</span>
-            19.99
+            {getPrice(pricing.basic)}
           </p>
 
           <ul className={styles.features}>
@@ -53,7 +96,7 @@ export default function PricingCard() {
 
           <p className={styles.price}>
             <span className={styles.currency}>$</span>
-            19.99
+            {getPrice(pricing.basic)}
           </p>
 
           <ul className={styles.features}>
