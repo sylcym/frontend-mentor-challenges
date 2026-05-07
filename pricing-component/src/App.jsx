0ceddb46408543c0e11bda4ad893c8813da69b7a
sylcym <prusak.syl@gmail.com>
2026-05-07 13:27:24 +0200

    code review and link attribution

--- pricing-component/src/App.jsx
+++ pricing-component/src/App.jsx
@@ -2,9 +2,32 @@ import PricingCard from "./features/pricing/components/PricingCard";
 
 function App() {
   return (
-    <div className="app">
-      <PricingCard />
-    </div>
+    <>
+      <div className="app">
+        <PricingCard />
+
+      </div>
+      <footer className="attribution">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          href=""
+          target="_blank"
+          rel="noreferrer"
+        >
+          sylcym
+        </a>
+        .
+      </footer>
+
+    </>
   );
 }
 
