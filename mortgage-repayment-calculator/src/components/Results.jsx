85ffab3cd86186eb6b7e6e63eba319a8f66018ad
sylcym <prusak.syl@gmail.com>
2026-06-07 10:28:11 +0200

    complete mortgage calculator UI

--- mortgage-repayment-calculator/src/components/Results.jsx
+++ mortgage-repayment-calculator/src/components/Results.jsx
@@ -1,9 +1,25 @@
 import './../styles/results.css'
+import emptyIllustration from '../assets/images/illustration-empty.svg'
 
 function Results() {
   return (
     <section className="results-section">
-      <h2 className='results-title'>Results</h2>
+      <div className="results-empty">
+        <img
+          src={emptyIllustration}
+          alt=""
+        />
+
+        <h2 className="results-title">
+          Results shown here
+        </h2>
+
+        <p className="results-description">
+          Complete the form and click “calculate
+          repayments” to see what your monthly
+          repayments would be.
+        </p>
+      </div>
     </section>
   )
 }
