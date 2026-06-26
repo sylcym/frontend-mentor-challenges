de2ccadc8ce1e425bb66e84be76042fb13370422
sylcym <prusak.syl@gmail.com>
2026-06-26 13:07:27 +0200

    Create mobile sidebar navigation

--- invoice-app/invoice-app/src/components/Sidebar.jsx
+++ invoice-app/invoice-app/src/components/Sidebar.jsx
@@ -1,9 +1,18 @@
-import "../styles/Sidebar.css"
+import '../styles/Sidebar.css'
 
 function Sidebar() {
   return (
     <aside className="aside">
-      Sidebar
+      <div className="sidebar-logo">
+        <div className="logo-circle"></div>
+      </div>
+
+      <div className="sidebar-avatar">
+        <img
+          src="https://i.pravatar.cc/40"
+          alt="User avatar"
+        />
+      </div>
     </aside>
   )
 }
