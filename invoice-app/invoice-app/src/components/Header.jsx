7e7695ad5160c1f1b8a0c78a7bebd167221c1213
sylcym <prusak.syl@gmail.com>
2026-06-26 13:14:51 +0200

    Improve mobile invoices header

--- invoice-app/invoice-app/src/components/Header.jsx
+++ invoice-app/invoice-app/src/components/Header.jsx
@@ -1,25 +1,22 @@
-// import InvoiceCard from '../components/InvoiceCard'
-import "../styles/Header.css"
+import '../styles/Header.css'
 
 function Header() {
   return (
-    <>
-      <header className="header">
-        <div>
-          <h1 className="header-title">
-            Invoices
-          </h1>
-          <p className="header-description">
-            There are 7 total invoices
-          </p>
-        </div>
+    <header className="header">
+      <div>
+        <h1 className="header-title">
+          Invoices
+        </h1>
 
-        <button className="add-button">
-          New
-        </button>
-      </header>
+        <p className="header-description">
+          7 invoices
+        </p>
+      </div>
 
-    </>
+      <button className="add-button">
+        + New
+      </button>
+    </header>
   )
 }
 
