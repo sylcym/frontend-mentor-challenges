99d4aebb33c5d8da7446efa3f9018d6e6177eb3f
sylcym <prusak.syl@gmail.com>
2026-06-30 14:36:49 +0200

    Add invoice form fields

--- invoice-app/invoice-app/src/components/InvoiceForm.jsx
+++ invoice-app/invoice-app/src/components/InvoiceForm.jsx
@@ -21,6 +21,58 @@ function InvoiceForm({ setShowInvoiceForm }) {
         <h2 className="form-title">
           New Invoice
         </h2>
+
+        <form className="invoice-form-content">
+          <section className="form-section">
+            <h3 className="section-title">
+              Bill From
+            </h3>
+
+            <div className="form-group">
+              <label className="form-label">
+                Street Address
+              </label>
+
+              <input
+                className="form-input"
+                type="text"
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="form-label">
+                City
+              </label>
+
+              <input
+                className="form-input"
+                type="text"
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="form-label">
+                Post Code
+              </label>
+
+              <input
+                className="form-input"
+                type="text"
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="form-label">
+                Country
+              </label>
+
+              <input
+                className="form-input"
+                type="text"
+              />
+            </div>
+          </section>
+        </form>
       </aside>
     </>
   )
