fdb0733850256bfba99321b009f53019a7c612ca
sylcym <prusak.syl@gmail.com>
2026-07-01 13:43:10 +0200

    Build invoice form sections

--- invoice-app/invoice-app/src/components/InvoiceForm.jsx
+++ invoice-app/invoice-app/src/components/InvoiceForm.jsx
@@ -7,7 +7,13 @@ function InvoiceForm({ setShowInvoiceForm }) {
     street: '',
     city: '',
     postCode: '',
-    country: ''
+    country: '',
+    clientName: '',
+    clientEmail: '',
+    clientStreet: '',
+    clientCity: '',
+    clientPostCode: '',
+    clientCountry: '',
   })
 
   function handleChange(e) {
@@ -98,6 +104,98 @@ function InvoiceForm({ setShowInvoiceForm }) {
                 type="text"
                 name="country"
                 value={formData.country}
+                onChange={handleChange}
+              />
+            </div>
+          </section>
+
+          <section className="form-section">
+            <h3 className="section-title">
+              Bill To
+            </h3>
+
+            <div className="form-group">
+              <label className="form-label">
+                Client Name
+              </label>
+
+              <input
+                className="form-input"
+                type="text"
+                name="clientName"
+                value={formData.clientName}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="form-label">
+                Client Email
+              </label>
+
+              <input
+                className="form-input"
+                type="email"
+                name="clientEmail"
+                value={formData.clientEmail}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="form-label">
+                Street Address
+              </label>
+
+              <input
+                className="form-input"
+                type="text"
+                name="clientStreet"
+                value={formData.clientStreet}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-row">
+              <div className="form-group">
+                <label className="form-label">
+                  City
+                </label>
+
+                <input
+                  className="form-input"
+                  type="text"
+                  name="clientCity"
+                  value={formData.clientCity}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="form-group">
+                <label className="form-label">
+                  Post Code
+                </label>
+
+                <input
+                  className="form-input"
+                  type="text"
+                  name="clientPostCode"
+                  value={formData.clientPostCode}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label className="form-label">
+                Country
+              </label>
+
+              <input
+                className="form-input"
+                type="text"
+                name="clientCountry"
+                value={formData.clientCountry}
                 onChange={handleChange}
               />
             </div>
