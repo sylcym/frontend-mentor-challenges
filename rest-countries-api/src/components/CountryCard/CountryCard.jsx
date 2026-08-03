add4d94d294f18f616439b5846d094231cfa2535
sylcym <prusak.syl@gmail.com>
2026-08-03 15:53:02 +0200

    Create country card component

---
+++ rest-countries-api/src/components/CountryCard/CountryCard.jsx
@@ -0,0 +1,51 @@
+import PropTypes from "prop-types";
+import "./CountryCard.css";
+
+function CountryCard({
+  flag,
+  name,
+  population,
+  region,
+  capital,
+}) {
+  return (
+    <article className="country-card">
+      <img
+        className="country-card-image"
+        src={flag}
+        alt={name}
+      />
+
+      <div className="country-card-content">
+        <h2 className="country-card-title">
+          {name}
+        </h2>
+
+        <p className="country-card-info">
+          <strong>Population:</strong> {population}
+        </p>
+
+        <p className="country-card-info">
+          <strong>Region:</strong> {region}
+        </p>
+
+        <p className="country-card-info">
+          <strong>Capital:</strong> {capital}
+        </p>
+      </div>
+    </article>
+  );
+}
+
+CountryCard.propTypes = {
+  flag: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  population: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+  ]).isRequired,
+  region: PropTypes.string.isRequired,
+  capital: PropTypes.string.isRequired,
+};
+
+export default CountryCard;
--- rest-countries-api/src/pages/Home.jsx
+++ rest-countries-api/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header/Header";
 import SearchBar from "../components/SearchBar/SearchBar";
 import Filter from "../components/Filter/Filter"
+import CountryCard from "../components/CountryCard/CountryCard";
 
 function Home() {
   return (
@@ -8,6 +9,13 @@ function Home() {
       <Header />
       <SearchBar />
       <Filter />
+      <CountryCard
+        flag="https://flagcdn.com/w320/de.png"
+        name="Germany"
+        population={81770900}
+        region="Europe"
+        capital="Berlin"
+      />
     </>
   );
 }
