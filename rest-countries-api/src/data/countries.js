0030abb9be86b7a9cbebc58e9242fc8400d81ec1
sylcym <prusak.syl@gmail.com>
2026-08-04 09:26:47 +0200

    Create countries grid component

---
+++ rest-countries-api/src/data/countries.js
@@ -0,0 +1,25 @@
+const countries = [
+  {
+    name: "Germany",
+    population: 81770900,
+    region: "Europe",
+    capital: "Berlin",
+    flag: "https://flagcdn.com/w320/de.png",
+  },
+  {
+    name: "France",
+    population: 66710000,
+    region: "Europe",
+    capital: "Paris",
+    flag: "https://flagcdn.com/w320/fr.png",
+  },
+  {
+    name: "Poland",
+    population: 37950000,
+    region: "Europe",
+    capital: "Warsaw",
+    flag: "https://flagcdn.com/w320/pl.png",
+  },
+];
+
+export default countries;
--- rest-countries-api/src/pages/Home.jsx
+++ rest-countries-api/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import Header from "../components/Header/Header";
 import SearchBar from "../components/SearchBar/SearchBar";
 import Filter from "../components/Filter/Filter"
-import CountryCard from "../components/CountryCard/CountryCard";
+import countries from "../data/countries";
+import CountriesGrid from "../components/CountriesGrid/CountriesGrid";
 
 function Home() {
   return (
@@ -9,13 +10,7 @@ function Home() {
       <Header />
       <SearchBar />
       <Filter />
-      <CountryCard
-        flag="https://flagcdn.com/w320/de.png"
-        name="Germany"
-        population={81770900}
-        region="Europe"
-        capital="Berlin"
-      />
+      <CountriesGrid countries={countries} />
     </>
   );
 }
