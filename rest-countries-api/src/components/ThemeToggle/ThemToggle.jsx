c3ea29fff4dee400de8613804f87ec24984e37e6
sylcym <prusak.syl@gmail.com>
2026-08-04 15:57:33 +0200

    Refactor project structure

--- rest-countries-api/src/components/ThemeToggle/ThemToggle.jsx
+++ rest-countries-api/src/components/ThemeToggle/ThemToggle.jsx
@@ -1,11 +1,19 @@
-import "./ThenToggle.css"
+import "./ThemToggle.css"
+import iconMoon from "../../assets/ikons/icon-moon.svg"
 
 function ThemeToggle() {
   return (
     <button className="theme-toggle" type="button">
-      <span className="theme-toggle-icon">🌙</span>
+      <img
+        className="theme-toggle-icon"
+        src={iconMoon}
+        alt=""
+        aria-hidden="true"
+      />
 
-      <span className="theme-toggle-text">Dark Mode</span>
+      <span className="theme-toggle-text">
+        Dark Mode
+      </span>
     </button>
   );
 }
