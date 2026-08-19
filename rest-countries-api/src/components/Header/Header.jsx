4415e7fc9d6a704d9a8704476e68b14b92dc247a
sylcym <prusak.syl@gmail.com>
2026-08-19 10:18:12 +0200

    Add home link to header

--- rest-countries-api/src/components/Header/Header.jsx
+++ rest-countries-api/src/components/Header/Header.jsx
@@ -1,12 +1,15 @@
 import "./Header.css";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import ThemeToggle from "../ThemeToggle/ThemToggle"
 
 function Header({ darkMode, setDarkMode }) {
   return (
     <header className="header">
       <div className="container header-container">
-        <h1 className="header-title">Where in the world?</h1>
+        <Link to="/" className="header-title-link">
+          <h1 className="header-title">Where in the world?</h1>
+        </Link>
 
         <ThemeToggle
           darkMode={darkMode}
