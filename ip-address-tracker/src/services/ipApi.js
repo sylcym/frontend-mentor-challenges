93a10db219139a3e5bd4d66f8335d2259c01e47d
sylcym <prusak.syl@gmail.com>
2026-09-25 08:43:02 +0200

    Add domain search support

--- ip-address-tracker/src/services/ipApi.js
+++ ip-address-tracker/src/services/ipApi.js
@@ -2,9 +2,13 @@ const BASE_URL = "https://geo.ipify.org/api/v2/country,city"
 
 const API_KEY = import.meta.env.VITE_IPIFY_API_KEY
 
-async function getIpData(ipAddress) {
+async function getIpData(searchValue) {
+  const isIpAddress = /^\d{1,3}(\.\d{1,3}){3}$/.test(searchValue)
+
+  const parameter = isIpAddress ? "ipAddress" : "domain"
+
   const response = await fetch(
-    `${BASE_URL}?apiKey=${API_KEY}&ipAddress=${ipAddress}`
+    `${BASE_URL}?apiKey=${API_KEY}&${parameter}=${searchValue}`
   )
 
   if (!response.ok) {
